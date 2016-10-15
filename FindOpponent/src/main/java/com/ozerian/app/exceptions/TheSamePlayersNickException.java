a9e355394fa83978e4c18ec97f4675f9ab40f05c
vio1990 <vio1990@ukr.net>
2016-10-15 13:58:20 +0300

    Delete another exceptions.

--- FindOpponent/src/main/java/com/ozerian/app/exceptions/TheSamePlayersNickException.java
+++
@@ -1,8 +0,0 @@
-package com.ozerian.app.exceptions;
-
-public class TheSamePlayersNickException extends Exception {
-
-    public TheSamePlayersNickException(String message) {
-        super(message);
-    }
-}
