893bf8a9e5ce0b500d168cd7da714bc91c26bacf
vio1990 <vio1990@ukr.net>
2016-10-15 14:00:36 +0300

    Addition of javadoc.

--- FindOpponent/src/main/java/com/ozerian/app/service/FindOpponentService.java
+++ FindOpponent/src/main/java/com/ozerian/app/service/FindOpponentService.java
@@ -3,6 +3,9 @@ package com.ozerian.app.service;
 import com.ozerian.app.exceptions.ThereIsNoOpponentException;
 import com.ozerian.app.model.Player;
 
+/**
+ * Interface for service with appropriate defined methods.
+ */
 public interface FindOpponentService {
 
     boolean registerReadyPlayer(Player newPlayer);
