2c4a0752b1fc786ef57eebcd7e3bf498371d9069
vio1990 <vio1990@ukr.net>
2016-10-15 13:59:39 +0300

    Addition exception to the method signature.

--- FindOpponent/src/main/java/com/ozerian/app/service/FindOpponentService.java
+++ FindOpponent/src/main/java/com/ozerian/app/service/FindOpponentService.java
@@ -1,10 +1,11 @@
 package com.ozerian.app.service;
 
+import com.ozerian.app.exceptions.ThereIsNoOpponentException;
 import com.ozerian.app.model.Player;
 
 public interface FindOpponentService {
 
     boolean registerReadyPlayer(Player newPlayer);
 
-    Player findOpponent(Player readyPlayer);
+    Player findOpponent(Player readyPlayer) throws ThereIsNoOpponentException;
 }
--- FindOpponent/src/main/java/com/ozerian/app/service/FindOpponentServiceImpl.java
+++ FindOpponent/src/main/java/com/ozerian/app/service/FindOpponentServiceImpl.java
@@ -1,8 +1,9 @@
 package com.ozerian.app.service;
 
+import com.ozerian.app.exceptions.ThereIsNoOpponentException;
 import com.ozerian.app.model.Player;
 
-import java.util.Set;
+import java.util.NavigableSet;
 import java.util.TreeSet;
 
 /**
@@ -11,7 +12,7 @@ import java.util.TreeSet;
  */
 public class FindOpponentServiceImpl implements FindOpponentService {
 
-    private Set<Player> readyPlayers;
+    private NavigableSet<Player> readyPlayers;
 
     public FindOpponentServiceImpl() {
         this.readyPlayers = new TreeSet<>();
@@ -19,15 +20,64 @@ public class FindOpponentServiceImpl implements FindOpponentService {
 
     @Override
     public boolean registerReadyPlayer(Player newPlayer) {
-        return false;
+
+        if (checkIfPlayerIsReady(newPlayer)) {
+            boolean addNewPlayer = readyPlayers.add(newPlayer);
+            return addNewPlayer;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean checkIfPlayerIsReady(Player newPlayer) {
+        if (newPlayer.isReady()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
-    public Player findOpponent(Player readyPlayer) {
-        return null;
+    public Player findOpponent(Player readyPlayer) throws ThereIsNoOpponentException {
+
+        if (readyPlayer != null) {
+            Player closestLessOrEqualPlayer = readyPlayers.floor(readyPlayer);
+            Player closestHigherPlayer = readyPlayers.higher(readyPlayer);
+            return findOpponentAllCases(readyPlayer, closestLessOrEqualPlayer, closestHigherPlayer);
+        } else {
+            throw new NullPointerException("Transferred player is null!");
+        }
     }
 
-    public Set<Player> getReadyPlayers() {
+    private Player findOpponentAllCases(Player readyPlayer, Player closestLessOrEqualPlayer, Player closestHigherPlayer) throws ThereIsNoOpponentException {
+
+        if (closestHigherPlayer == null && closestLessOrEqualPlayer == null) {
+            throw new ThereIsNoOpponentException("There are no ready opponents!");
+        } else if (closestHigherPlayer == null && closestLessOrEqualPlayer != null) {
+            readyPlayers.remove(closestLessOrEqualPlayer);
+            return closestLessOrEqualPlayer;
+        } else if (closestHigherPlayer != null && closestLessOrEqualPlayer == null) {
+            readyPlayers.remove(closestHigherPlayer);
+            return closestHigherPlayer;
+        } else {
+            return findDeltaAndOpponent(readyPlayer, closestLessOrEqualPlayer, closestHigherPlayer);
+        }
+    }
+
+    private Player findDeltaAndOpponent(Player readyPlayer, Player closestLessOrEqualPlayer, Player closestHigherPlayer) {
+        double lessOrEqualDelta = readyPlayer.getRating() - closestLessOrEqualPlayer.getRating();
+        double higherDelta = closestHigherPlayer.getRating() - readyPlayer.getRating();
+
+        if (lessOrEqualDelta > higherDelta) {
+            readyPlayers.remove(closestHigherPlayer);
+            return closestHigherPlayer;
+        } else {
+            readyPlayers.remove(closestLessOrEqualPlayer);
+            return closestLessOrEqualPlayer;
+        }
+    }
+
+    public NavigableSet<Player> getReadyPlayers() {
         return readyPlayers;
     }
 
