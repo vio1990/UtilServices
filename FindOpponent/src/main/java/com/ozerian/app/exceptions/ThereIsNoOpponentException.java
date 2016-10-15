4a26669f47a4c212e0b0743b9dd83fa3f362c8c9
vio1990 <vio1990@ukr.net>
2016-10-15 14:01:18 +0300

    Addition of javadoc.

--- FindOpponent/src/main/java/com/ozerian/app/exceptions/ThereIsNoOpponentException.java
+++ FindOpponent/src/main/java/com/ozerian/app/exceptions/ThereIsNoOpponentException.java
@@ -1,5 +1,9 @@
 package com.ozerian.app.exceptions;
 
+/**
+ * This exception occurs when there is no opponents in the set
+ * with another players.
+ */
 public class ThereIsNoOpponentException extends Exception {
 
     public ThereIsNoOpponentException(String message) {
